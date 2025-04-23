470c0162db94a8e2502d7dc46e5e8ceb95570f7b
Yashwanth <mvyashwanthkumar@gmail.com>
2025-04-23 16:57:31 +0530

    Keep track of ones and zeros

---
+++ 1000/swap_and_delete.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        int count_ones=0;
+        int count_zeros=0;
+        
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='0')count_zeros++;
+            else count_ones++;
+        }
+        
+        int ts=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]== '0' && count_ones > 0){
+                count_ones--;
+                ts++;
+            }
+            else if(s[i]== '1' && count_zeros > 0){
+                count_zeros--;
+                ts++;
+            }
+            else 
+                break;
+        }
+        cout << s.length() - ts << endl;
+    }
+    return 0;
+}
